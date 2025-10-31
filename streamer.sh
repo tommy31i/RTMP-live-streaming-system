03409ff6d08258edb1b48ff3421b5739c3133982
tommy31i <kskprivate235@gmail.com>
2025-10-31 11:22:11 +0900

    bug fix

--- streamer.sh
+++ streamer.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# トークンファイル読み込み
-source ~/token.env
+# 外部ファイルから4つの変数を読み込み
+source ./token.env
 
-FILE="/home/ubuntu/A.mp4"
+# 入力ファイルの確認
+ffprobe -hide_banner -v error -show_streams "$FILE"
 
-ffmpeg -re -stream_loop -1 -i "$FILE" \
+# まずはプライマリだけで疎通確認（tee無し）
+ffmpeg -re -i "$FILE" \
   -c:v libx264 -preset veryfast -pix_fmt yuv420p -profile:v high -level 4.2 \
   -r 30 -g 60 -keyint_min 60 -b:v 4500k -maxrate 5000k -bufsize 10000k \
   -c:a aac -b:a 160k -ar 48000 -ac 2 \
-  -f tee "[f=flv]${URL_PRIMARY%/}/${YT_KEY}|[f=flv]${URL_BACKUP%/}/${YT_KEY}"
+  -f flv "${URL_PRIMARY%/}/$YT_KEY"
+
+# 成功したら、バックアップ同時配信（tee）
+ffmpeg -re -i "$FILE" \
+  -c:v libx264 -preset veryfast -pix_fmt yuv420p -profile:v high -level 4.2 \
+  -r 30 -g 60 -keyint_min 60 -b:v 4500k -maxrate 5000k -bufsize 10000k \
+  -c:a aac -b:a 160k -ar 48000 -ac 2 \
+  -f tee "[f=flv]${URL_PRIMARY%/}/$YT_KEY|[f=flv]${URL_BACKUP%/}/$YT_KEY"
